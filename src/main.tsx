99206827b0c57d79bfe79b15e84cca24d75055b2
kall1012 <kleber.quattro@gmail.com>
2026-05-15 14:41:02 -0300

    Correção Router Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/AraujoeAlves_lp">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
